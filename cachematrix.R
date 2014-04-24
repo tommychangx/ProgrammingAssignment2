c54929d21a43d5669de697260bb847ba7fa922c7
tommychangx <tommychangx@gmail.com>
2014-04-25 00:20:23 +0800

    PA2

--- cachematrix.R
+++ cachematrix.R
@@ -8,14 +8,14 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-		m <- NULL
-		set <- function(y) {
-             x <<- y
-             m <<- NULL
-		}
-		get <- function() x
-		setinverse <- function(inverse) m <<- inverse
-		getinverse <- function() m
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
